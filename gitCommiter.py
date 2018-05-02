f183a07c99e170f1fa0a285de528def0b876e2d6
suarezlui <suarezluis@gmail.com>
2018-05-02 14:19:52 -0500

    "commiter auto"

---
+++ gitCommiter.py
@@ -0,0 +1,23 @@
+from subprocess import call
+
+commits = 16
+filename = "delete"
+extention = ".me"
+pusher = "git push https://pthacler93:Peter@g27@github.com/pthacker93/conspam"
+
+def git():
+    call(["git", "add", "."])
+    call(["git", "commit", "-m",'"commiter auto"']) 
+    call(["git", "push", "https://pthacker93:Peter@g27@github.com/pthacker93/conspam"])
+   
+
+
+
+
+for i in range(0,commits):
+    file = filename + str(i) + extention
+    call(["touch", file])
+    git()
+    
+call(["rm", "*.me"])
+
